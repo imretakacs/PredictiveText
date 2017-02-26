ec600415fba641d76092a17ae8363a5382fca41a
Jimmy <Jimmy@DESKTOP-U2VD8H1>
2017-02-26 17:57:24 +0000

    Use f# ode from c# proj, run autocomplete from f# console

---
+++ FSharpConsole/Program.fs
@@ -0,0 +1,7 @@
+﻿open FSharp.TV.PredictiveText
+
+[<EntryPoint>]
+let main argv = 
+    let candidiates = Autocomplete "aa" (LoadDict())
+    for candidate in candidiates do printfn "%A" candidate
+    0 // return an integer exit code
---
+++ UseFSharpInCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FSharp.TV;
+
+namespace UseFSharpInCSharp
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var dict = PredictiveText.LoadDict();
+         foreach (var candidates in PredictiveText.Autocomplete("aa", dict))
+         {
+            Console.WriteLine(candidates);
+         }
+         Console.ReadKey();
+      }
+   }
+}
