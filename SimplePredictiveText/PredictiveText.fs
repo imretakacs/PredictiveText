7466200b26fcc2a98e2c9a092dfcff115888092b
Jimmy <Jimmy@DESKTOP-U2VD8H1>
2017-02-26 13:29:39 +0000

    Autocomplete from a dict

---
+++ SimplePredictiveText/PredictiveText.fs
@@ -0,0 +1,23 @@
+﻿module FSharp.TV.PredictiveText
+open System.IO
+open System
+
+let usDict = Path.Combine(__SOURCE_DIRECTORY__, "dict.txt")
+
+let loadDictFromPath path =
+   File.ReadAllLines path
+
+let loadDict () : string array = 
+   let dic : string array = loadDictFromPath usDict
+   dic
+
+let loaded : string array = loadDict()
+
+let filtering =
+   let data = [|"butyl";"buyer";"buzzy";"bwana";"bylaw";"byron";"bytes"|]
+   let candidtes = data |> Array.filter (fun word -> word.Contains "w")
+   candidtes
+
+let autocomplete (prefix:string) (dict:string[]) =
+   let candidtes = dict |> Array.filter (fun word -> word.StartsWith prefix)
+   candidtes
