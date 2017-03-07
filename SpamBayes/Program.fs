36e67af4afaea1193210b3c1a93bf614fa9e830e
Jimmy <Jimmy@DESKTOP-U2VD8H1>
2017-03-07 19:45:58 +0000

    final changes

--- SpamBayes/Program.fs
+++ SpamBayes/Program.fs
@@ -3,6 +3,14 @@ open System.IO
 
 type Label = Spam | Ham
 
+type Corpus = {
+   spamFreqs:Map<string,int>;
+   hamFreqs:Map<string,int>;
+   spamCount: int;
+   hamCount: int;
+   totalCount:int;
+}
+
 let classify (msg:string) : Label =
    Spam
 
@@ -23,6 +31,56 @@ let makeRandomPredicate fractionTrue =
       r.NextDouble() < fractionTrue
    predicate
 
+let features (msg:string) : seq<string> =
+   msg.Split( [|' '|] ) |> Array.toSeq
+
+let countLabel (label:Label) (labeledWords:seq<Label*string>) : int =
+   labeledWords
+   |> Seq.filter(fun (l,w) -> l = label)
+   |> Seq.length
+    
+let frequencies label labeledWords : Map<string, int> =
+   labeledWords
+   |> Seq.filter (fun (l,w) -> l=label)
+   |> Seq.groupBy (fun (l,w) -> w)
+   |> Seq.map (fun (w,ws)-> (w, Seq.length ws))
+   |> Map.ofSeq
+
+let pHam (corpus:Corpus) : float=
+   (float corpus.hamCount) / (float corpus.totalCount)
+
+let pSpam (corpus:Corpus) : float=
+   (float corpus.spamCount) / (float corpus.totalCount)
+
+let getCount map word =
+   match (Map.tryFind word map) with
+   | Some(x) -> (float x)
+   | _ -> 0.0001
+
+let pWordGivenSpam word (corpus:Corpus) : float =
+   (getCount corpus.spamFreqs word) / (float corpus.spamCount)
+
+let pWordGivenHam word (corpus:Corpus) : float =
+   (getCount corpus.hamFreqs word) / (float corpus.hamCount)
+
+let pWord word (corpus:Corpus) : float =
+   (pWordGivenHam word corpus)*(pHam corpus) + (pWordGivenSpam word corpus)*(pSpam corpus)
+
+
+let pHamGivenWords words corpus =
+   let product =
+      words
+      |> Seq.map (fun w-> (pWordGivenHam w corpus) / (pWord w corpus))
+      |> Seq.reduce (*)
+   product* (pHam corpus)
+
+let pSpamGivenWords words corpus =
+   let product =
+      words
+      |> Seq.map (fun w-> (pWordGivenSpam w corpus) / (pWord w corpus))
+      |> Seq.reduce (*)
+   product* (pSpam corpus)
+
 [<EntryPoint>]
 let main argv = 
     let path = Path.Combine(__SOURCE_DIRECTORY__, "SMSSpamCollection.txt")
@@ -37,8 +95,32 @@ let main argv =
       labelData
       |> Seq.toList 
       |> List.partition(makeRandomPredicate 0.8)
+    let words : seq<Label*seq<string>> =
+      training
+      |> Seq.map(fun (label, msg) -> (label, features msg))
 
+    let labeledWords : seq<Label*string> =
+      words
+      |>Seq.collect (fun (l, words) -> Seq.map(fun w -> (l,w)) words)
+
+    let corpus = {
+      spamFreqs = (frequencies Spam labeledWords);
+      hamFreqs = (frequencies Ham labeledWords);
+      hamCount = (countLabel Ham labeledWords);
+      spamCount = (countLabel Spam labeledWords);
+      totalCount = (Seq.length labeledWords);
+    }
+
+    let BayesClassify (msg:string) : Label =
+      let words = features msg
+      let pHam = pHamGivenWords words corpus
+      let pSpam = pSpamGivenWords words corpus
+      if pHam > pSpam then Ham else Spam
+
+    
     printfn "Training size: %A" training.Length
-    printfn "The accuracy is %A" (accuracyRate classify labelData)
+    printfn "The accuracy is %A" (accuracyRate BayesClassify validation)
+    printfn "Total=%A, Ham=%A, Spam=%A" corpus.totalCount corpus.hamCount corpus.spamCount
+    printfn "'the' occurs %A in Ham" (corpus.hamFreqs.Item "the")
     let value = Console.ReadKey()
     0 // return an integer exit code
